515e2f7d4fa94e4823fd41afab1cc03d7c35c402
Gama11 <jensfischer95@gmail.com>
2016-10-12 22:42:16 +0200

    CompletionItem: add command and additionalTextEdits
    (added in 2.6.0)

--- src/languageServerProtocol/Types.hx
+++ src/languageServerProtocol/Types.hx
@@ -710,6 +710,20 @@ typedef CompletionItem = {
     @:optional var textEdit:TextEdit;
 
     /**
+        An optional array of additional text edits that are applied when
+        selecting this completion. Edits must not overlap with the main edit
+        nor with themselves.
+    **/
+    @:optional var additionalTextEdits:Array<TextEdit>;
+
+    /**
+        An optional command that is executed *after* inserting this completion. *Note* that
+        additional modifications to the current document should be described with the
+        additionalTextEdits-property.
+    **/
+    @:optional var commnad:Command;
+
+    /**
         An data entry field that is preserved on a completion item between a completion and a completion resolve request.
     **/
     @:optional var data:Dynamic;
